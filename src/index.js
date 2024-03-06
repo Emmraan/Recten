b177592cf5ae018c53df0a55a89d1b8174009210
emmraan <emran805110@gmail.com>
2024-03-06 21:05:06 +0530

    from react-dom to  change react-dom/client

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/main/main";
 import Work from "./pages/Work/work";
